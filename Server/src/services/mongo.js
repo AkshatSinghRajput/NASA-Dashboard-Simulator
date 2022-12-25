e3fbd8b3851f186710f8a6684145e2a6961edcc9
AkshatSinghRajput <choturajput0@gmail.com>
2022-12-25 19:13:19 +0530

    CI-updated

--- Server/src/services/mongo.js
+++ Server/src/services/mongo.js
@@ -10,7 +10,7 @@ mongoose.connection.on("error", (err) => {
 });
 
 async function connectMongo() {
-  await mongoose.connect(process.env.MONGO_URI_2, {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
